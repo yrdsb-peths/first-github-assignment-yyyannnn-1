74712f0d968224143fd40704bfd54389c18e3627
yyyannnn <150150320+yyyannnn@users.noreply.github.com>
2024-09-05 12:46:50 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+  private String firstName;
+  private String lastName;  
+
+  public Student(String fName, String lName, int grade)
+  {
+        firstName = firstName;
+        lastName = lastName;
+  }
+
+  public String toString()
+  {
+       return firstName + " " + lastName;
+  }
+    
+}
