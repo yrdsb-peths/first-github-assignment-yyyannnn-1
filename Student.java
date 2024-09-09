9aea52bda175d2130acd7780bb6a0fd00ead75aa
yyyannnn <150150320+yyyannnn@users.noreply.github.com>
2024-09-09 12:53:55 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,12 +2,14 @@ public class Student {
   private String firstName;
   private String lastName; 
   private int grade; 
+  public static int totalStudents;
 
   public Student(String fName, String lName, int grade)
   {
         firstName = firstName;
         lastName = lastName;
         this.grade = grade;
+	totalStudents++;
   }
 
   public String toString()
